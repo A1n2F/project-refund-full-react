f323c6d279656b972726ea7820d3c5eb0321c519
A1n2F <alissonfferreira22@gmail.com>
2025-04-29 21:18:14 -0300

    Utilizando o Zod para validação dos campos de cadastro

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,29 +1,82 @@
 import { useState } from "react"
+import { z, ZodError } from "zod"
+
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+
+const singUpSchema = z.object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "Email inválido" }),
+    password: z.string().min(6, { message: "Senha deve conter pelo menos 6 dígitos" }),
+    passwordConfirm: z.string({ message: "Confirme a senha" })
+}).refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais.",
+    path: ["passwordConfirm"]
+})
 
 export function SignUp() {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [passwordConfirm, setPasswordConfirm] = useState("")
-    const [isLoading, setIsloading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
     function onSubmit(e: React.FormEvent) {
         e.preventDefault()
         
-        console.log(name, email, password, passwordConfirm)
+        try {
+            setIsLoading(true)
+
+            const data = singUpSchema.parse({
+                name,
+                email,
+                password,
+                passwordConfirm
+            })
+            
+        } catch (error) {
+            if(error instanceof ZodError) {
+                return alert(error.issues[0].message)
+            }
+
+            alert("Não foi possível cadastrar.")
+        } finally {
+            setIsLoading(false)
+        }
     }
 
     return (
         <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
-            <Input required legend="Nome" type="text" placeholder="Seu nome" onChange={(e) => setName(e.target.value)} />
+            <Input 
+                required 
+                legend="Nome" 
+                type="text" 
+                placeholder="Seu nome" 
+                onChange={(e) => setName(e.target.value)}
+            />
 
-            <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e) => setEmail(e.target.value)} />
+            <Input 
+                required 
+                legend="E-mail" 
+                type="email" 
+                placeholder="seu@email.com" 
+                onChange={(e) => setEmail(e.target.value)} 
+            />
 
-            <Input required legend="Senha" type="password" placeholder="123456" onChange={(e) => setPassword(e.target.value)} />
+            <Input 
+                required 
+                legend="Senha" 
+                type="password" 
+                placeholder="123456" 
+                onChange={(e) => setPassword(e.target.value)} 
+            />
 
-            <Input required legend="Confirmação da Senha" type="password" placeholder="123456" onChange={(e) => setPasswordConfirm(e.target.value)} />
+            <Input 
+                required 
+                legend="Confirmação da Senha" 
+                type="password" placeholder="123456" 
+                onChange={(e) => setPasswordConfirm(e.target.value)} 
+            />
 
             <Button type="submit" isLoading={isLoading}>Cadastrar</Button>
 
