3ddae51500791959cf96cc348dd1f4a9e3245861
A1n2F <alissonfferreira22@gmail.com>
2025-05-05 15:45:17 -0300

    Carregando dados da solicitação

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router"
 import { AxiosError } from "axios"
 
@@ -14,6 +14,7 @@ import { Input } from "../components/Input"
 import { Select } from "../components/Select"
 import { Upload } from "../components/Upload"
 import { Button } from "../components/Button"
+import { formatCurrency } from "../utils/formatCurrency"
 
 const refundSchema = z.object({
     name: z.string().min(3, { message: "Informe um nome claro para sua solicitação" }),
@@ -27,6 +28,7 @@ export function Refund() {
     const [category, setCategory] = useState("")
     const [isLoading, setIsloading] = useState(false)
     const [file, setFile] = useState<File | null>(null)
+    const [fileURL, setFileURL] = useState<string | null>(null)
 
     const navigate = useNavigate()
     const params = useParams<{id: string}>()
@@ -77,6 +79,32 @@ export function Refund() {
 
     }
 
+    async function fetchRefund(id: string) {
+        try {
+            const { data } = await api.get<RefundAPIResponse>(`/refunds/${id}`)
+            
+            setName(data.name)
+            setCategory(data.category)
+            setAmount(formatCurrency(data.amount))
+            setFileURL(data.filename)
+
+        } catch (error) {
+            console.log(error)
+
+            if(error instanceof AxiosError) {
+                return alert(error.response?.data.message)
+            }
+
+            alert("Não foi possível carregar")
+        }
+    }
+
+    useEffect(() => {
+        if(params.id) {
+            fetchRefund(params.id)
+        }
+    }, [params.id])
+
     return (
         <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
                 <header>
@@ -102,8 +130,8 @@ export function Refund() {
             </div>
 
             {
-                params.id ? (
-                    <a href="https://app.rocketseat.com.br/" 
+                params.id && fileURL ? (
+                    <a href={`http://localhost:3333/uploads/${fileURL}`} 
                     target="_blank" 
                     className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 
                     hover:opacity-70 transition ease-linear">
